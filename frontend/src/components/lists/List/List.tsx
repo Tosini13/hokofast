fa6fc7dc03d62fb7bdd73fe4fa1c75c56de4c807
Tosini13 <jbartos13@gmail.com>
2022-04-10 02:21:22 +0200

    added item form

--- frontend/src/components/lists/List/List.tsx
+++ frontend/src/components/lists/List/List.tsx
@@ -1,9 +1,28 @@
-import { CircularProgress, Stack, Typography } from "@mui/material";
-import React from "react";
+import { Add } from "@mui/icons-material";
+import {
+  CircularProgress,
+  Dialog,
+  DialogTitle,
+  IconButton,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
+import React, { useCallback, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useItemsService } from "../../../services/items/item-service";
+import {
+  createItem,
+  useItemsService,
+} from "../../../services/items/item-service";
 import { Id } from "../../../types/utils";
-import ItemList from "./ItemList";
+import Item from "../../items/Item";
+import ItemForm from "../../items/ItemForm";
+
+const DialogStyled = styled(Dialog)`
+  .MuiPaper-root {
+    padding: 10px;
+  }
+`;
 
 const withId = (Component: React.ComponentType<TListProps>) => {
   return () => {
@@ -20,15 +39,34 @@ type TListProps = {
 };
 
 const List: React.FC<TListProps> = ({ listId }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const items = useItemsService(listId);
+
+  const handleSubmit = useCallback(
+    (name: string) => {
+      createItem(listId, { name });
+    },
+    [listId]
+  );
+
+  console.log("isOpen", isOpen);
+
   return (
     <>
       <Typography>List</Typography>
       <Stack spacing={1} style={{ margin: "0px 20px" }}>
         {items?.map((item) => (
-          <ItemList key={item.id} item={item} />
+          <Item key={item.id} item={item} />
         ))}
+        <Stack direction={"row"} alignItems="center" justifyContent={"center"}>
+          <IconButton onClick={() => setIsOpen(!isOpen)}>
+            <Add />
+          </IconButton>
+        </Stack>
       </Stack>
+      <DialogStyled open={isOpen} onClose={() => setIsOpen(false)}>
+        <ItemForm handleSubmit={handleSubmit} />
+      </DialogStyled>
     </>
   );
 };
